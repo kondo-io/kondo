d293342242aa471571c9e8f10d3736e0e19f634d
Ken Erwin <ken.erwin@traderev.com>
2019-04-20 19:47:19 -0400

    updated with more testing

--- kondo/detector/detect_language_type.py
+++ kondo/detector/detect_language_type.py
@@ -1,12 +1,20 @@
 import glob
+import os
+import logging
 
 
 def detect_repository_type(path):
+    """Return language type of a given folder/path"""
+    log = logging.getLogger(__name__)
     languages = [
         {"java": [".java"]},
         {"python": [".py"]},
         {"terraform": [".tf", ".tfvars"]}
     ]
+    # Check to see if path exists
+    if not os.path.isdir(path):
+        log.exception("Path passed in to detect_repository_type is invalid")
+        return False
     file_type_count = {}
     for language in languages:
         for name, extensions in language.items():
